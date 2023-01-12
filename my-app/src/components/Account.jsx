9ce397d6354c037c3a7b1687a00872ec4f35af3f
arielniscola <41024148+arielniscola@users.noreply.github.com>
2023-01-12 04:58:55 -0300

    Revert "Update Account.jsx"

--- my-app/src/components/Account.jsx
+++ my-app/src/components/Account.jsx
@@ -1,26 +1,24 @@
 import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { Navigate } from 'react-router-dom';
 import OwnerDashboard from "./OwnerDashboard";
 import UserDashBoard from "./UserDashboard";
-import Develop from "./DeveloperDashBoard";
+import Develop from "./DeveloperDashBoard"
 
 const Account = () => {
   const currentUser = useSelector((state) => state.currentUser);
 
   if (!currentUser) return <Navigate to="/login" replace />;
 
-  if (currentUser?.rol === "admin") return <Develop />;
+  const dashBoard = () => {
+    if(currentUser.rol === "admin") return <Develop/>
+    if(currentUser.rol === "owner") return <OwnerDashboard/>
+  }
 
+  
   return (
     <>
-      {currentUser.rol === "owner" ? (
-        <>
-          <UserDashBoard />
-          <OwnerDashboard />
-        </>
-      ) : (
-        <UserDashBoard />
-      )}
+    { currentUser.rol !== "admin" && <UserDashBoard />}
+    {dashBoard()}
     </>
   );
 };
